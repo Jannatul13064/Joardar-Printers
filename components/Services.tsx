87d687c4c2b448f61cd7cf1648e0d16cf1a91f67
Jannatul13064 <akzzislam19194@gmail.com>
2026-02-26 11:10:29 +0600

    immage optimized

--- components/Services.tsx
+++ components/Services.tsx
@@ -21,6 +21,7 @@ export default function Services() {
       {/* Services Grid */}
       <motion.div
         className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto"
+        initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.3 }}
         variants={{
