bff499b44260581c793c88212f52f68f33c9d6a0
Jannatul13064 <akzzislam19194@gmail.com>
2026-02-26 15:48:59 +0600

    added

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Portfolio from "@/components/Portfolio";
 import Contact from "@/components/Contact";
-
+import Clients from "@/components/Clients";
 import About from "@/components/About";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Clients />
       <About />
       <Services />
       <Portfolio />
---
+++ components/Clients.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function ClientSlider() {
+  const logos = [
+    "/clients/client1.png",
+    "/clients/client2.png",
+    "/clients/client3.png",
+    "/clients/client4.png",
+    "/clients/client5.png",
+  ];
+
+  return (
+    <section className="py-16 overflow-hidden">
+      <div className="text-center mb-10">
+        <h2 className=" text-white text-3xl font-bold">
+          Trusted by Leading Companies
+        </h2>
+      </div>
+
+      <div className="relative w-full overflow-hidden">
+        <motion.div
+          className="flex gap-12"
+          animate={{ x: ["0%", "-100%"] }}
+          transition={{
+            repeat: Infinity,
+            duration: 30,
+            ease: "linear",
+          }}
+        >
+          {[...logos, ...logos].map((logo, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-center min-w-[200px]"
+            >
+              <Image
+                src={logo}
+                alt="client logo"
+                width={140}
+                height={80}
+                className="object-contain grayscale-0 hover:grayscale transition"
+              />
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </section>
+  );
+}
