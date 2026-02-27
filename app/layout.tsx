d937e208cd5ad5fb4caf8283f05bfcc3bd2b5e17
Akash Islam <akzzislam19194@gmail.com>
2026-02-27 23:44:47 +0600

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,30 +55,6 @@ export default function RootLayout({
         className={`${poppins.className} antialiased scroll-smooth bg-black text-white relative overflow-x-hidden`}
       >
         {/* GLOBAL BACKGROUND EFFECT */}
-        <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
-          {/* Gradient Base */}
-          <div className="absolute inset-0 bg-gradient-to-b from-black via-gray-900 to-black" />
-
-          {/* RED GLOW */}
-          <div className="absolute -top-40 -left-40 w-[600px] h-[600px] bg-red-500/30 blur-[180px] animate-pulse" />
-
-          {/* YELLOW GLOW */}
-          <div className="absolute bottom-[-200px] right-[-200px] w-[600px] h-[600px] bg-yellow-400/30 blur-[180px] animate-pulse" />
-
-          {/* CENTER LIGHT SPOT */}
-          <div className="absolute top-1/2 left-1/2 w-[700px] h-[700px] -translate-x-1/2 -translate-y-1/2 bg-white/5 blur-[220px]" />
-
-          {/* RADIAL SPOTLIGHT */}
-          <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.08),transparent_60%)]" />
-
-          {/* GRID PATTERN */}
-          <div
-            className="absolute inset-0 opacity-[0.04]
-            bg-[linear-gradient(to_right,#ffffff20_1px,transparent_1px),
-            linear-gradient(to_bottom,#ffffff20_1px,transparent_1px)]
-            bg-[size:40px_40px]"
-          />
-        </div>
 
         {/* HEADER */}
         <Navbar />
